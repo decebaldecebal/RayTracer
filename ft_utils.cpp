f0e35245f93aba8b0d1c9985b909a785e50767b8
Rares Serban <rares_serban96@yahoo.com>
2015-03-18 11:43:17 +0200

    Update ft_utils.cpp

--- ft_utils.cpp
+++ ft_utils.cpp
@@ -18,27 +18,6 @@ void	mem_error(void)
 	exit(-1);
 }
 
-void	get_sx_sy(float *sx, float *sy, int x, int y)
-{
-	if (WIN_WIDTH > WIN_HEIGHT)
-	{
-		*sx = ((x + 0.5) / WIN_WIDTH) * ASPECT -
-			(((WIN_WIDTH - WIN_HEIGHT) / (float)WIN_HEIGHT) / 2);
-		*sy = ((WIN_HEIGHT - y) + 0.5) / WIN_HEIGHT;
-	}
-	else if (WIN_HEIGHT > WIN_WIDTH)
-	{
-		*sx = (x + 0.5) / (float)WIN_WIDTH;
-		*sy = (((WIN_HEIGHT - y) + 0.5) / WIN_HEIGHT) / ASPECT -
-			(((WIN_HEIGHT - WIN_WIDTH) / (float)WIN_WIDTH) / 2);
-	}
-	else
-	{
-		*sx = (x + 0.5) / WIN_WIDTH;
-		*sy = ((WIN_HEIGHT - y) + 0.5) / WIN_HEIGHT;
-	}
-}
-
 void	get_normal(t_vec3 *v, t_obj *o, t_vec3 *vec)
 {
 	t_vec3	temp;
