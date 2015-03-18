1f44c44178a04760f9f3771d71b28b990c7644ba
Rares Serban <rares_serban96@yahoo.com>
2015-03-18 11:38:47 +0200

    Update ft_free.cpp

--- ft_free.cpp
+++ ft_free.cpp
@@ -41,3 +41,59 @@ void        free_ray(t_ray **ray)
     free(*ray);
     *ray = NULL;
 }
+
+static void free_object(t_obj *obj)
+{
+    free(obj->mat);
+    obj->mat = NULL;
+    free(obj->normal);
+    obj->normal = NULL;
+    if (obj->type == cyl)
+        free(((t_cylinder *)obj->obj)->dir);
+    else if (obj->type == cone)
+        free(((t_cone *)obj->obj)->dir);
+    free(obj->obj);
+    obj->obj = NULL;
+}
+
+static void free_camera(t_camera *cam)
+{
+    free(cam->camdir);
+    cam->camdir = NULL;
+    free(cam->camdown);
+    cam->camdown = NULL;
+    free(cam->campos);
+    cam->campos = NULL;
+    free(cam->camright);
+    cam->camright = NULL;
+}
+
+void        free_environment(t_env *e)
+{
+    int i;
+
+    i = 0;
+    while (e->objs[i])
+    {
+        free_object(e->objs[i]);
+        free(e->objs[i]);
+        e->objs[i] = NULL;
+        i++;
+    }
+    free(e->objs);
+    e->objs = NULL;
+    i = 0;
+    while (e->lights[i])
+    {
+        free(e->lights[i]->pos);
+        free(e->lights[i]);
+        i++;
+    }
+    free(e->lights);
+    e->lights = NULL;
+    free(e->img);
+    e->img = NULL;
+    free_camera(e->cam);
+    free(e->cam);
+    e->cam = NULL;
+}
